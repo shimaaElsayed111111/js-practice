97359d8d621e2033ac4885137649a01801179e38
shimaaElsayed <shimaashima1121999@gmail.com>
2025-08-26 22:36:43 +0300

    'counter'

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// add remove to-do list tasks
 var inputText =document.getElementById('txt');
 
     function addItem() {
@@ -18,3 +19,38 @@ var inputText =document.getElementById('txt');
         alert("list is empty");
     }
 }
+
+
+// counter app
+let count = 0;
+    const counter = document.getElementById('counter');
+    const incBtn = document.getElementById('incBtn');
+    const decBtn = document.getElementById('decBtn');
+    const reset = document.getElementById('reset');
+
+    
+    function myTasksCounter() {
+        counter.textContent = count;
+    }
+
+    
+    incBtn.addEventListener('click', () => {
+        count++;
+        myTasksCounter();
+    });
+
+    decBtn.addEventListener('click', () => {
+        if(count > 0)
+        {
+            count--;
+        }
+        myTasksCounter();
+    });
+
+    reset.addEventListener('click', () => {
+        count = 0;
+        myTasksCounter();
+    });
+
+    // intial display
+    myTasksCounter();
